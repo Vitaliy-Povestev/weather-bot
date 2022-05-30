47ac3c1f830081b2b8f043e4c878208da0899ccc
Vitaliy-Povestev <vitaliypovestev@gmail.com>
2022-05-30 22:58:54 +0300

    да сука ебаный хуеку блядь

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require("express");
 require("dotenv").config();
 
@@ -24,28 +23,3 @@ app.post(`/${process.env.TELEGRAM_TOKEN}`, (req, res) => {
 app.listen(port, () => {
   console.log(`\n\nServer running on port ${port}.\n\n`);
 });
-=======
-const express = require("express");
-require("dotenv").config();
-
-const bot = require("./bot");
-
-const app = express();
-
-const port = process.env.PORT || 5000;
-
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.status(200).json({ message: "Hello from the Bot API." });
-});
-// TELEGRAM WEBHOOK - https://core.telegram.org/bots/api#setwebhook
-app.post(`/${process.env.TELEGRAM_TOKEN}`, (req, res) => {
-  bot.processUpdate(req.body);
-  res.status(200).json({ message: "ok" });
-});
-
-app.listen(port, () => {
-  console.log(`\n\nServer running on port ${port}.\n\n`);
-});
->>>>>>> 2918df2af6c622d810f35955c02073f89e5ee85d
