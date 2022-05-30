2918df2af6c622d810f35955c02073f89e5ee85d
Veseliy_start <97013944+Vitaliy-Povestev@users.noreply.github.com>
2022-05-30 19:49:48 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+require("dotenv").config();
+
+const bot = require("./bot");
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Hello from the Bot API." });
+});
+// TELEGRAM WEBHOOK - https://core.telegram.org/bots/api#setwebhook
+app.post(`/${process.env.TELEGRAM_TOKEN}`, (req, res) => {
+  bot.processUpdate(req.body);
+  res.status(200).json({ message: "ok" });
+});
+
+app.listen(port, () => {
+  console.log(`\n\nServer running on port ${port}.\n\n`);
+});
